c7c178ff5d9408f9e17c27db58cf11a11401539e
Zander Martineau <mrmartineau@users.noreply.github.com>
2023-09-24 22:40:04 +0100

    Hide FAB button on new/edit pages

--- src/components/FabAdd.tsx
+++ src/components/FabAdd.tsx
@@ -1,11 +1,22 @@
 'use client';
 
 import { PlusCircle } from '@phosphor-icons/react';
+import { usePathname, useSelectedLayoutSegments } from 'next/navigation';
 
 import { ROUTE_NEW_BOOKMARK } from '../constants';
 import { Link } from './Link';
 
 export const FabAdd = () => {
+  const pathname = usePathname();
+  const segments = useSelectedLayoutSegments();
+
+  const hideFab =
+    pathname === `/new/bookmark` || segments[segments.length - 1] === 'edit';
+
+  if (hideFab) {
+    return null;
+  }
+
   return (
     <Link href={ROUTE_NEW_BOOKMARK} variant="fab" aria-label="Add new bookmark">
       <PlusCircle size="30" weight="duotone" color="currentColor" />
