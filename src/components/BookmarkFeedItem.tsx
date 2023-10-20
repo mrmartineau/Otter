9e4fe96553d68ffe046a26ae7f25f44f7c1e11d8
Zander Martineau <mrmartineau@users.noreply.github.com>
2023-10-20 21:51:18 +0100

    Improve image display in feed

--- src/components/BookmarkFeedItem.tsx
+++ src/components/BookmarkFeedItem.tsx
@@ -20,50 +20,56 @@ export const BookmarkFeedItem = (props: BookmarkFeedItemProps) => {
 
   return (
     <div className="feed-item-wrapper">
-      {url && title ? (
+      <Flex gap="m" justify="between">
         <div>
-          <Link
-            href={url}
-            variant="feedTitle"
-            onClick={() => handleClickRegister(id)}
-          >
-            {title}
-          </Link>
+          {url && title ? (
+            <div>
+              <Link
+                href={url}
+                variant="feedTitle"
+                onClick={() => handleClickRegister(id)}
+              >
+                {title}
+              </Link>
+            </div>
+          ) : title ? (
+            <div>{title}</div>
+          ) : null}
+          {description ? <Markdown>{description}</Markdown> : null}
+          {note ? (
+            <div className="rounded bg-theme4 p-xs">
+              <Flex
+                gap="2xs"
+                align="center"
+                className="mb-s border-b-2 border-solid border-theme7 pb-3xs text-step--1"
+              >
+                <Note
+                  aria-label="Note"
+                  weight="duotone"
+                  size={20}
+                  className="shrink-0 text-theme10"
+                />
+                Note
+              </Flex>
+              <Markdown>{note}</Markdown>
+            </div>
+          ) : null}
+          {tweet?.text ? (
+            <Flex gap="2xs">
+              <div className="shrink-0 text-theme10">
+                <TwitterLogo size={18} weight="duotone" />
+              </div>
+              <div className="shrink-0 text-step--1 tracking-tight">
+                <Link href={tweet.url}>@{tweet.username}</Link>
+                <Markdown>{tweet.text}</Markdown>
+              </div>
+            </Flex>
+          ) : null}
         </div>
-      ) : title ? (
-        <div>{title}</div>
-      ) : null}
-      {description ? <Markdown>{description}</Markdown> : null}
-      {image ? <img src={image} alt="" className="feed-image" /> : null}
-      {note ? (
-        <div className="rounded bg-theme4 p-xs">
-          <Flex
-            gap="2xs"
-            align="center"
-            className="mb-s border-b-2 border-solid border-theme7 pb-3xs text-step--1"
-          >
-            <Note
-              aria-label="Note"
-              weight="duotone"
-              size={20}
-              className="shrink-0 text-theme10"
-            />
-            Note
-          </Flex>
-          <Markdown>{note}</Markdown>
+        <div>
+          {image ? <img src={image} alt="" className="feed-image" /> : null}
         </div>
-      ) : null}
-      {tweet?.text ? (
-        <Flex gap="2xs">
-          <div className="shrink-0 text-theme10">
-            <TwitterLogo size={18} weight="duotone" />
-          </div>
-          <div className="shrink-0 text-step--1 tracking-tight">
-            <Link href={tweet.url}>@{tweet.username}</Link>
-            <Markdown>{tweet.text}</Markdown>
-          </div>
-        </Flex>
-      ) : null}
+      </Flex>
       <FeedItemFooter {...props} key={`footer-${id}`} />
     </div>
   );
