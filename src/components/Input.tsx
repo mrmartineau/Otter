b9b40ab4e42323b69583f5f3eda622a713a1010a
Zander Martineau <mrmartineau@users.noreply.github.com>
2023-11-02 19:26:48 +0000

    Update input styles

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -2,7 +2,7 @@ import { cn } from '@/src/utils/classnames';
 import * as React from 'react';
 
 export const sharedInputStyles =
-  'w-full rounded-m border-0 bg-theme3 px-2xs py-3xs text-step-0 text-theme10 ring-offset-background placeholder:text-theme7 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-theme9 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50';
+  'w-full rounded-m border-0 bg-theme5 px-2xs py-3xs text-step-0 text-theme10 ring-offset-background placeholder:text-theme8 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-theme9 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50';
 
 export interface InputProps
   extends React.InputHTMLAttributes<HTMLInputElement> {}
