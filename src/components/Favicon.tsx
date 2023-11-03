056914feccbe88db88bc337ca6052b989eed1c17
Zander Martineau <mrmartineau@users.noreply.github.com>
2023-11-03 11:51:15 +0000

    Update old otter emoji in search

--- src/components/Favicon.tsx
+++ src/components/Favicon.tsx
@@ -24,8 +24,7 @@ export const Favicon = ({ url }: FaviconProps) => {
       className="block shrink-0 overflow-hidden rounded-full"
       onError={(event) => {
         event.currentTarget.onerror = null; // prevents looping
-        event.currentTarget.src =
-          'data:image/svg+xml,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20100%20100%22%3E%3Ctext%20y%3D%22.9em%22%20font-size%3D%2290%22%3E%F0%9F%A6%A6%3C%2Ftext%3E%3C%2Fsvg%3E';
+        event.currentTarget.src = '/otter-logo.svg';
       }}
     />
   );
