6fbbcea95d9b0cfbf9488df5a0f75b6ed34f7acb
Zander Martineau <mrmartineau@users.noreply.github.com>
2023-12-20 23:24:20 +0000

    Remove tldjs dependency and update simpleUrl implementation

--- src/utils/simpleUrl.ts
+++ src/utils/simpleUrl.ts
@@ -1,9 +1,9 @@
 import memoizeOne from 'memoize-one';
-import tldjs from 'tldjs';
 
-export const simpleUrlSource = (url: string): string => {
-  const parsedUrl = tldjs.parse(url);
-  return parsedUrl.hostname?.replace('www.', '') || url;
-};
-
-export const simpleUrl = memoizeOne(simpleUrlSource);
+export const simpleUrl = memoizeOne(
+  (url: string, withPath?: boolean): string => {
+    const newUrl = new URL(url);
+    const hostname = newUrl.hostname?.replace('www.', '');
+    return withPath ? `${hostname}${newUrl.pathname}` : hostname;
+  },
+);
