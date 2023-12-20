ca324b300efc80f4e16b68693bdf7674eb2bdd59
Zander Martineau <mrmartineau@users.noreply.github.com>
2023-12-20 23:51:11 +0000

    Fix LogoutButton styles to match other sidebar items

--- src/components/LogoutButton.tsx
+++ src/components/LogoutButton.tsx
@@ -1,16 +1,13 @@
 import { SignOut } from '@phosphor-icons/react/dist/ssr';
 
 import { CONTENT } from '../constants';
-import { Flex } from './Flex';
 
 export default function LogoutButton() {
   return (
     <form action="/auth/sign-out" method="post">
-      <button className="link-base focus link-sidebar block">
-        <Flex gapX="xs" align="center">
-          <SignOut aria-label="Sign out" size={18} weight="duotone" />
-          {CONTENT.signOutNav}
-        </Flex>
+      <button className="link-base focus link-sidebar w-full !justify-start !gap-3xs">
+        <SignOut aria-label="Sign out" size={18} weight="duotone" />
+        {CONTENT.signOutNav}
       </button>
     </form>
   );
