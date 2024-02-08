9e3fb5a4769743831648cf31146749cc387bb5d1
Zander Martineau <mrmartineau@users.noreply.github.com>
2024-02-08 19:04:15 +0000

    Ensure app can't be scaled when on mobile

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,18 @@
+import type { Viewport } from 'next'
 import './globals.css';
 
 export const metadata = {
   title: 'Otter',
   description: 'Bookmarking on your own terms',
 };
+
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+  colorScheme: 'dark light',
+}
 
 export default function RootLayout({
   children,
