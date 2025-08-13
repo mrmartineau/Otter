353bdd47ebaf17d2e306bd5edf36b2bbcce7484c
Zander Martineau <mrmartineau@users.noreply.github.com>
2025-08-14 00:46:36 +0200

    Improve loading

--- packages/web/src/components/Loader.tsx
+++ packages/web/src/components/Loader.tsx
@@ -1,3 +1,14 @@
 export const Loader = () => {
   return <div>Loading...</div>
 }
+
+export const FullLoader = () => {
+  return (
+    <div className="loader flex items-center justify-center h-dvh">
+      <div className="flex flex-col items-center gap-2">
+        <img src="/icon.svg" alt="Otter" className="w-20 h-20" />
+        <div className="text-2xl font-bold">Loading Otter&hellip;</div>
+      </div>
+    </div>
+  )
+}
