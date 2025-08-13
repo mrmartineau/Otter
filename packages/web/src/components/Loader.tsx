f85edf2fc682604414a8a9be4bae464fd9d79d20
Zander Martineau <mrmartineau@users.noreply.github.com>
2025-08-14 01:35:22 +0200

    everybody use the same logo svg

--- packages/web/src/components/Loader.tsx
+++ packages/web/src/components/Loader.tsx
@@ -6,7 +6,7 @@ export const FullLoader = () => {
   return (
     <div className="loader flex items-center justify-center h-dvh">
       <div className="flex flex-col items-center gap-2">
-        <img src="/icon.svg" alt="Otter" className="w-20 h-20" />
+        <img src="/otter-logo.svg" alt="Otter" className="w-20 h-20" />
         <div className="text-2xl font-bold">Loading Otter&hellip;</div>
       </div>
     </div>
